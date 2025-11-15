12a0a5d8b527327493b26f262a7a44cbac72a3f9
p0m067j <paulo.miranda0@walmart.com>
2025-11-14 22:35:24 -0300

    Added options

--- src/data.js
+++ src/data.js
@@ -1,89 +1,6 @@
 export const THEMES_DATA = {
-  nba: {
-    name: 'Jugadores de NBA',
-    singular: 'jugador de NBA',
-    gender: 'o',
-    items: [
-      'Michael Jordan', 'LeBron James', 'Kobe Bryant', 'Magic Johnson', 'Larry Bird',
-      'Kareem Abdul-Jabbar', 'Tim Duncan', 'Shaquille O\'Neal', 'Wilt Chamberlain', 'Bill Russell',
-      'Stephen Curry', 'Kevin Durant', 'Hakeem Olajuwon', 'Oscar Robertson', 'Jerry West',
-      'Julius Erving', 'Moses Malone', 'Dirk Nowitzki', 'Karl Malone', 'Charles Barkley',
-      'John Stockton', 'Scottie Pippen', 'Dwyane Wade', 'Kevin Garnett', 'Allen Iverson',
-      'Steve Nash', 'Jason Kidd', 'Chris Paul', 'Clyde Drexler', 'David Robinson',
-      'Patrick Ewing', 'Isiah Thomas', 'Ray Allen', 'Reggie Miller', 'Paul Pierce',
-      'Kawhi Leonard', 'Giannis Antetokounmpo', 'Nikola Jokic', 'Luka Doncic', 'Damian Lillard',
-      'James Harden', 'Russell Westbrook', 'Anthony Davis', 'Carmelo Anthony', 'Pau Gasol',
-      'Tony Parker', 'Manu Ginobili', 'Vince Carter', 'Tracy McGrady', 'Yao Ming'
-    ]
-  },
-  soccer: {
-    name: 'Jugadores de Fútbol',
-    singular: 'jugador de fútbol',
-    gender: 'o',
-    items: [
-      'Lionel Messi', 'Cristiano Ronaldo', 'Pelé', 'Diego Maradona', 'Johan Cruyff',
-      'Zinedine Zidane', 'Ronaldo Nazário', 'Ronaldinho', 'Franz Beckenbauer', 'Alfredo Di Stéfano',
-      'Michel Platini', 'Marco van Basten', 'George Best', 'Bobby Charlton', 'Lev Yashin',
-      'Gerd Müller', 'Paolo Maldini', 'Franco Baresi', 'Lothar Matthäus', 'Roberto Baggio',
-      'Romário', 'Rivaldo', 'Thierry Henry', 'Andrés Iniesta', 'Xavi Hernández',
-      'Neymar Jr', 'Kylian Mbappé', 'Erling Haaland', 'Robert Lewandowski', 'Karim Benzema',
-      'Luka Modrić', 'Kevin De Bruyne', 'Mohamed Salah', 'Sergio Ramos', 'Virgil van Dijk',
-      'Manuel Neuer', 'Gianluigi Buffon', 'Iker Casillas', 'Luis Suárez', 'Sergio Agüero',
-      'Zlatan Ibrahimović', 'Wayne Rooney', 'Frank Lampard', 'Steven Gerrard', 'Andrea Pirlo',
-      'Gareth Bale', 'Eden Hazard', 'Sadio Mané', 'Harry Kane', 'Son Heung-min'
-    ]
-  },
-  food: {
-    name: 'Comidas Típicas',
-    singular: 'comida',
-    gender: 'a',
-    items: [
-      'Pizza', 'Sushi', 'Tacos', 'Hamburguesa',
-      'Paella', 'Pad Thai', 'Ramen', 'Curry Indio',
-      'Asado de vacuno', 'Ceviche', 'Empanada de Pino', 'Croissant Dulce',
-      'Falafel', 'Kebab', 'Lasaña', 'Risotto', 'Gyoza',
-      'Porotos con rienda', 'Asado de cordero', 'Biryani', 'Shawarma', 'Completos',
-      'Pastel de Choclo', 'Empanada Frita de Queso', 'Empanada Frita de Mariscos', 'Pescado Frito', 'Cazuela de Ave',
-      'Cazuela de Vacuno', 'Fideos con Salsa de Tomate', 'Bistec con Arroz', 'Bistec con Papas Fritas', 'Pollo Arvejado',
-      'Puré con Vienesas', 'Bistec a lo Pobre', 'Barros Jarpa', 'Choripán', 'Sopaipillas',
-      'Sashimi', 'Tempura', 'Ají de gallina', 'Pan con Manjar', 'Pan con Palta',
-      'Ñoquis', 'Ravioles', 'Pan con mortadela', 'Pastel de papas', 'Churros'
-    ]
-  },
-  series: {
-    name: 'Series de TV',
-    singular: 'serie',
-    gender: 'a',
-    items: [
-      'Breaking Bad', 'Game of Thrones', 'The Sopranos', 'The Wire', 'Friends',
-      'The Office', 'Stranger Things', 'The Crown', 'Sherlock', 'Black Mirror',
-      'Westworld', 'True Detective', 'Mad Men', 'Lost', 'The Walking Dead',
-      'Dexter', 'House of Cards', 'Narcos', 'Peaky Blinders', 'Money Heist',
-      'Dark', 'Chernobyl', 'Band of Brothers', 'The Mandalorian', 'The Witcher',
-      'Vikings', 'Better Call Saul', 'Fargo', 'Twin Peaks', 'The X-Files',
-      'Seinfeld', 'Curb Your Enthusiasm', 'Parks and Recreation', 'Brooklyn Nine-Nine', 'Community',
-      'Rick and Morty', 'BoJack Horseman', 'Arcane', 'The Last of Us', 'Succession',
-      'The Boys', 'Invincible', 'Fleabag', 'The Handmaid\'s Tale', 'Ozark',
-      'Mindhunter', 'Squid Game', 'Wednesday', 'The Bear', 'Severance'
-    ]
-  },
-  metalChile: {
-    name: 'Bandas de Metal Chileno',
-    singular: 'banda de metal chilena',
-    gender: 'a',
-    items: [
-      'Pentagram', 'Criminal', 'Dorso', 'Atomic Aggressor', 'Torture',
-      'Sadism', 'Masacre', 'Totten Korps', 'Coffin Curse', 'Orator',
-      'Unholyforce', 'Thornafire', 'Demonic Rage', 'Cancerbero', 'Demoniac',
-      'Warpath', 'Poema Arcanus', 'Mar de Grises', 'Procession', 'Lethal',
-      'Panzer', 'Battlerage', 'Rancor', 'Vastator', 'Force of Darkness',
-      'Communion', 'Grave Desecrator', 'Wrathprayer', 'Slaughtbbath', 'Godless',
-      'Parkcrest', 'DevilPreacher', 'Fosa Común', 'Machuca', 'Yajaira',
-      'Inanna', 'Kuervos del Sur', 'Escombro', 'Dogma', 'Dekapited',
-      'Eternal Sacrifice', 'Ripper', 'Soulfly Chile', 'Thy Antichrist', 'BlackStorm',
-      'Darkness', 'Inherited', 'Iron Attack', 'Legacy of Brutality', 'Steel Bearing Hand'
-    ]
-  },
+  // ======== METAL ========
+
   metal: {
     name: 'Bandas de Metal Internacional',
     singular: 'banda de metal',
@@ -101,23 +18,77 @@ export const THEMES_DATA = {
       'Dream Theater', 'Tool', 'System of a Down', 'Rammstein', 'Nightwish'
     ]
   },
-  movies: {
-    name: 'Películas',
-    singular: 'película',
+
+  metalChile: {
+    name: 'Bandas de Metal Chileno',
+    singular: 'banda de metal chilena',
     gender: 'a',
     items: [
-      'The Godfather', 'The Shawshank Redemption', 'Pulp Fiction', 'The Dark Knight', 'Schindler\'s List',
-      'Forrest Gump', 'Fight Club', 'Inception', 'The Matrix', 'Goodfellas',
-      'Star Wars', 'The Lord of the Rings', 'Interstellar', 'The Silence of the Lambs', 'Saving Private Ryan',
-      'Se7en', 'The Departed', 'Gladiator', 'The Prestige', 'The Green Mile',
-      'Memento', 'Apocalypse Now', 'The Usual Suspects', 'American History X', 'Léon: The Professional',
-      'The Pianist', 'Parasite', 'Whiplash', 'The Shining', 'Alien',
-      'Blade Runner', 'Terminator 2', 'Jurassic Park', 'Back to the Future', 'Die Hard',
-      'Mad Max: Fury Road', 'No Country for Old Men', 'There Will Be Blood', 'Drive', 'The Grand Budapest Hotel',
-      'Her', 'La La Land', 'Jojo Rabbit', 'Knives Out', 'Everything Everywhere All at Once',
-      'Oppenheimer', 'Dune', 'The Batman', '1917', 'Joker'
+      'Pentagram', 'Criminal', 'Dorso', 'Atomic Aggressor', 'Torture',
+      'Sadism', 'Masacre', 'Totten Korps', 'Coffin Curse', 'Orator',
+      'Unholyforce', 'Thornafire', 'Demonic Rage', 'Cancerbero', 'Demoniac',
+      'Warpath', 'Poema Arcanus', 'Mar de Grises', 'Procession', 'Lethal',
+      'Panzer', 'Battlerage', 'Rancor', 'Vastator', 'Force of Darkness',
+      'Communion', 'Grave Desecrator', 'Wrathprayer', 'Slaughtbbath', 'Godless',
+      'Parkcrest', 'DevilPreacher', 'Fosa Común', 'Machuca', 'Yajaira',
+      'Inanna', 'Kuervos del Sur', 'Escombro', 'Dogma', 'Dekapited',
+      'Eternal Sacrifice', 'Ripper', 'Soulfly Chile', 'Thy Antichrist', 'BlackStorm',
+      'Darkness', 'Inherited', 'Iron Attack', 'Legacy of Brutality', 'Steel Bearing Hand'
     ]
   },
+
+  metalAlbums: {
+    name: 'Discos de Metal',
+    singular: 'disco de metal',
+    gender: 'o',
+    items: [
+      'Master of Puppets (Metallica)',
+      'Reign in Blood (Slayer)',
+      'Painkiller (Judas Priest)',
+      'Paranoid (Black Sabbath)',
+      'Rust in Peace (Megadeth)',
+      'Blackwater Park (Opeth)',
+      'Leviathan (Mastodon)',
+      'At the Heart of Winter (Immortal)',
+      'Altars of Madness (Morbid Angel)',
+      'Symbolic (Death)',
+      'Crimson (Edge of Sanity)',
+      'Powerslave (Iron Maiden)',
+      'De Mysteriis Dom Sathanas (Mayhem)',
+      'In the Nightside Eclipse (Emperor)',
+      'Transilvanian Hunger (Darkthrone)',
+      'The Bleeding (Cannibal Corpse)',
+      'Left Hand Path (Entombed)',
+      'Like an Ever Flowing Stream (Dismember)',
+      'Pierced from Within (Suffocation)',
+      'Epicus Doomicus Metallicus (Candlemass)',
+      'Black One (Sunn O)))',
+      'Ghost Reveries (Opeth)',
+      'Lateralus (Tool)',
+      'Crack the Skye (Mastodon)'
+    ]
+  },
+
+  // ======== FÚTBOL ========
+
+  soccer: {
+    name: 'Jugadores de Fútbol',
+    singular: 'jugador de fútbol',
+    gender: 'o',
+    items: [
+      'Lionel Messi', 'Cristiano Ronaldo', 'Pelé', 'Diego Maradona', 'Johan Cruyff',
+      'Zinedine Zidane', 'Ronaldo Nazário', 'Ronaldinho', 'Franz Beckenbauer', 'Alfredo Di Stéfano',
+      'Michel Platini', 'Marco van Basten', 'George Best', 'Bobby Charlton', 'Lev Yashin',
+      'Gerd Müller', 'Paolo Maldini', 'Franco Baresi', 'Lothar Matthäus', 'Roberto Baggio',
+      'Romário', 'Rivaldo', 'Thierry Henry', 'Andrés Iniesta', 'Xavi Hernández',
+      'Neymar Jr', 'Kylian Mbappé', 'Erling Haaland', 'Robert Lewandowski', 'Karim Benzema',
+      'Luka Modrić', 'Kevin De Bruyne', 'Mohamed Salah', 'Sergio Ramos', 'Virgil van Dijk',
+      'Manuel Neuer', 'Gianluigi Buffon', 'Iker Casillas', 'Luis Suárez', 'Sergio Agüero',
+      'Zlatan Ibrahimović', 'Wayne Rooney', 'Frank Lampard', 'Steven Gerrard', 'Andrea Pirlo',
+      'Gareth Bale', 'Eden Hazard', 'Sadio Mané', 'Harry Kane', 'Son Heung-min'
+    ]
+  },
+
   soccerTeams: {
     name: 'Equipos de Fútbol',
     singular: 'equipo de fútbol',
@@ -133,6 +104,63 @@ export const THEMES_DATA = {
       'Peñarol', 'Nacional', 'Independiente', 'Racing', 'San Lorenzo',
       'Colo-Colo', 'Universidad de Chile', 'Universidad Católica', 'América', 'Chivas',
       'Galatasaray', 'Fenerbahçe', 'Al Ahly', 'Kaizer Chiefs', 'LA Galaxy'
+    ]
+  },
+
+  soccerChile: {
+    name: 'Equipos de Fútbol Chileno',
+    singular: 'equipo de fútbol chileno',
+    gender: 'o',
+    items: [
+      'Colo-Colo', 'Universidad de Chile', 'Universidad Católica', 'Unión Española',
+      'Palestino', 'Audax Italiano', 'Cobresal', 'Huachipato', 'Everton',
+      'Coquimbo Unido', 'O’Higgins', 'Curicó Unido', 'Deportes La Serena',
+      'Deportes Antofagasta', 'Ñublense', 'Deportes Iquique',
+
+      'Cobreloa', 'Santiago Wanderers', 'Magallanes', 'Barnechea',
+      'Santiago Morning', 'San Luis de Quillota', 'Deportes Puerto Montt',
+      'Deportes Temuco', 'Rangers', 'Unión San Felipe', 'Deportes Recoleta',
+      'Deportes Santa Cruz',
+
+      'General Velásquez', 'Deportes Valdivia', 'Deportes Concepción',
+      'Real San Joaquín', 'Independiente de Cauquenes', 'Trasandino',
+      'Rodelindo Román', 'Limache', 'Melipilla'
+    ]
+  },
+
+  // ======== NUEVAS CATEGORÍAS ========
+
+  cars: {
+    name: 'Automóviles',
+    singular: 'automóvil',
+    gender: 'o',
+    items: [
+      'Mazda CX-30', 'Mazda 3', 'Volkswagen Gol', 'Volkswagen Polo',
+      'Toyota Corolla', 'Toyota Yaris', 'Toyota RAV4', 'Honda Civic',
+      'Honda Fit', 'Kia Morning', 'Kia Rio', 'Kia Sportage',
+      'Hyundai Tucson', 'Hyundai Elantra', 'Hyundai Accent',
+      'Chevrolet Sail', 'Chevrolet Spark', 'Chevrolet Tracker',
+      'Nissan Versa', 'Nissan Kicks', 'Nissan X-Trail',
+      'Suzuki Swift', 'Suzuki Baleno', 'Suzuki Vitara',
+      'Ford Focus', 'Ford Fiesta', 'Ford Explorer',
+      'Jeep Renegade', 'Jeep Compass', 'Subaru Forester', 'Subaru Outback'
+    ]
+  },
+
+  drinks: {
+    name: 'Tragos',
+    singular: 'trago',
+    gender: 'o',
+    items: [
+      'Piscola', 'Piscola con blanca', 'Piscola con negra',
+      'Cerveza', 'Michelada', 'Terremoto',
+      'Ponche de melón', 'Ponche de durazno',
+      'Whisky-cola', 'Ron-cola', 'Vodka-naranja',
+      'Jagerbomb', 'Fernet con Coca', 'Mojito',
+      'Pisco Sour', 'Cola de mono', 'Vermut',
+      'Pipeño', 'Chicha', 'Borgoña', 'Melón con vino',
+      'Aperol Spritz', 'Vodka tónica', 'Gin Tonic', 'Grapa', 'Chuflai', 'Chupilca del diaulo',
+      'Chicha de manzana', 'Sangría'
     ]
   }
 };
